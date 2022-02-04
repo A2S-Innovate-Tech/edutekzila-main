caa8013bc524b51d26f240fa886473d893636135
deepak <trojanhorsedc@gmail.com>
2022-02-04 23:51:08 +0530

    Sidebar  created, bug fixes

---
+++ src/sidebarContents.js
@@ -0,0 +1,170 @@
+const sidebarContents = 
+    {
+        "Tech Services": {
+            "Mobile App Development" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            "Web Development" : [
+                "Frontend Development",
+                "Backend Development",
+                "Full Stack Development",
+                "Swift Development",
+            ],
+            "Cross Platform Development" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Cloud Computing" : [
+                "Cloud Management",
+                "Cloud Services",
+            ],
+            "Graphic Designing" : [
+                "Logo Designing",
+                "UI / UX Designing",
+            ],
+            "Other Services" : [
+                "Digital Marketing",
+                "Video Editing",
+            ]
+        },
+
+        "Edu Services": {
+            "Edu Service 1" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            "Edu Service 2" : [
+                "Frontend Development",
+                "Backend Development",
+                "Full Stack Development",
+                "Swift Development",
+            ],
+            "Edu Service 3" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Edu Service 4" : [
+                "Cloud Management",
+                "Cloud Services",
+            ],
+            "Edu Service 5" : [
+                "Logo Designing",
+                "UI / UX Designing",
+            ],
+        },
+
+        "Extensions": {
+            "Extension 1" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            "Extension 2" : [
+                "Frontend Development",
+                "Backend Development",
+                "Full Stack Development",
+                "Swift Development",
+            ],
+            "Extension 3" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Extension 4" : [
+                "Cloud Management",
+                "Cloud Services",
+            ],
+            "Extension 5" : [
+                "Logo Designing",
+                "UI / UX Designing",
+            ],
+            "Extension 6" : [
+                "Digital Marketing",
+                "Video Editing",
+            ]
+        },
+
+        "Solutions": {
+            "Solution 1" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            
+            "Solution 3" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Solution 4" : [
+                "Cloud Management",
+                "Cloud Services",
+            ],
+            "Solution 5" : [
+                "Logo Designing",
+                "UI / UX Designing",
+            ],
+            "Other Solutions" : [
+                "Digital Marketing",
+                "Video Editing",
+            ]
+        },
+
+        "Company": {
+            "Company 1" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            "Company 2" : [
+                "Frontend Development",
+                "Backend Development",
+                "Full Stack Development",
+                "Swift Development",
+            ],
+            "Company 3" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Company 4" : [
+                "Logo Designing",
+                "UI / UX Designing",
+            ],
+            "Company 5" : [
+                "Digital Marketing",
+                "Video Editing",
+            ]
+        },
+
+        "Careers": {
+            "Career 1" : [
+                "Android Development",
+                "iOS Development",
+                "Flutter Development",
+                "Swift Development",
+            ],
+            "Career 2" : [
+                "Frontend Development",
+                "Backend Development",
+                "Full Stack Development",
+                "Swift Development",
+            ],
+            "Career 3" : [
+                "Ionic Development",
+                "React Native Development",
+            ],
+            "Career 4" : [
+                "Cloud Management",
+                "Cloud Services",
+            ],
+        },
+
+    };
+
+export default sidebarContents;

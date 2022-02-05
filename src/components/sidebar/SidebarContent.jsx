3536c4c885f854d0db3d8e41314a52c575b72758
deepak <trojanhorsedc@gmail.com>
2022-02-06 02:14:03 +0530

    Dynamic navigation added for sidebar

--- src/components/sidebar/SidebarContent.jsx
+++ src/components/sidebar/SidebarContent.jsx
@@ -4,10 +4,13 @@ import "./SidebarContent.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircle} from '@fortawesome/free-solid-svg-icons';
 import TriangleImg from "../../assets/images/triangle.png";
+import {Link} from "react-router-dom";
 
-const SidebarContent = ({content}) =>{
+const SidebarContent = ({content,serviceCategory,setToggleSidebar}) =>{
     // const screenWidthState = useContext(ScreenWidthContext);
-    
+    const handleToggleSidebar =()=>{
+        setToggleSidebar(false);
+    }
     return <>
         <div className="Sidebar-Content Grid">
             <img src={TriangleImg} alt="arrow" />
@@ -21,7 +24,7 @@ const SidebarContent = ({content}) =>{
                                 entry[1].map((listItem)=>{
                                     return <div key={listItem} className="List-Item">
                                         <FontAwesomeIcon className="Bullet" icon={ faCircle } size="sm"/>
-                                        <p key={listItem}>{listItem}</p>
+                                        <Link onClick={handleToggleSidebar} key={listItem} to={serviceCategory+'/'+entry[0]+'/'+listItem}>{listItem} </Link>  
                                     </div>
                                 })
                             }
--- src/pages/ParticularService.jsx
+++ src/pages/ParticularService.jsx
@@ -9,25 +9,28 @@ import OurWork from "../components/homeComponents/ourWork/OurWork";
 import ClientsTestinomials from "../components/homeComponents/clientsTestinomials/ClientsTestinomials";
 import OurBlogs from "../components/homeComponents/ourBlogs/OurBlogs";
 import ContactUs from "../components/homeComponents/contactUs/ContactUs";
+import { useLocation } from "react-router";
 
 const ParticularService = () =>{
-    
+    const location = useLocation();
+    // console.log(location.pathname.split('/')[2]=="Android%20Development")
+    console.log(decodeURI(location.pathname).split("/")[1]);
+    const serviceName = decodeURI(location.pathname).split("/")[3];
+    const serviceType = decodeURI(location.pathname).split("/")[2];
     return <div>
         <div className="Home-Container">
             {/* WE WORK ON 4D PROCESS         */}
             <div className="Home-Sub-Container Top-Heading">
                 <h2>
-                    WE, WORK ON 4D PROCESS
+                    OUR {serviceType.toUpperCase()} SERVICE
                 </h2>
-                <h1 className = "Light-Blue">
-                    DISCUSS
-                </h1>
-                <h1>
-                    <span className = "Light-Blue">DESIGN</span> DEVELOP
-                </h1>
-                <h1 className = "Light-Blue">
-                    DELIVER
-                </h1>
+                {
+                    serviceName.trim().split(" ").map((txt)=>{
+                        return <h1 className = "Light-Blue">
+                            {txt}
+                        </h1>
+                    })
+                }
             </div>
 
             {/* SUCCESS STORIES   */}
